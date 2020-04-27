44b63110b25e7c667e91385ed83ad6dfe65dcec8
Kevin Estrella <kevsestrella@gmail.com>
2020-04-27 18:10:54 +0800

    Added implementation of IR feeding to IECore

--- feed_network.py
+++ feed_network.py
@@ -1,5 +1,7 @@
 import argparse
 ### TODO: Load the necessary libraries
+import os
+from openvino.inference_engine import IENetwork, IECore
 
 CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
 
@@ -22,19 +24,28 @@ def get_args():
 
 def load_to_IE(model_xml, cpu_ext):
     ### TODO: Load the Inference Engine API
+    plugin = IECore()
 
     ### TODO: Load IR files into their related class
+    bin = os.path.splitext(model_xml)[0] + '.bin'
+    network = IENetwork(model=model_xml, weights=bin)
 
     ### TODO: Add a CPU extension, if applicable. It's suggested to check
     ###       your code for unsupported layers for practice before 
     ###       implementing this. Not all of the models may need it.
+    # plugin.add_extension(cpu_ext, "CPU")
 
     ### TODO: Get the supported layers of the network
-
+    supported_layers = plugin.query_network(network=network, device_name="CPU")
+    unsupported_layers = [l for l in network.layers.keys() if l not in supported_layers]
     ### TODO: Check for any unsupported layers, and let the user
     ###       know if anything is missing. Exit the program, if so.
+    if len(unsupported_layers):
+        print(f"Unsupported layers: {unsupported_layers}")
+        exit(1)
 
     ### TODO: Load the network into the Inference Engine
+    plugin.load_network(network, "CPU")
 
     print("IR successfully loaded into Inference Engine.")
 
