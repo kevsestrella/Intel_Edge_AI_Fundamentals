60b974cb7ab54181268e6c9a26eebbe08b8b2ec8
Kevin Estrella <kevsestrella@gmail.com>
2020-04-27 12:41:40 +0800

    Added function for general preprocessing

--- preprocess_inputs.py
+++ preprocess_inputs.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 
+def preprocess_image(image, height, width):
+    preprocessed_image = cv2.resize(image, (width, height))
+    preprocessed_image = preprocessed_image.transpose(2,0,1)
+    preprocessed_image = preprocessed_image.reshape(1,3,height,width)
+
+    return preprocessed_image
+
+
 def pose_estimation(input_image):
     '''
     Given some input image, preprocess the image so that
