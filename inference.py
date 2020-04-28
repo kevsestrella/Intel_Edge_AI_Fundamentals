e25ab896b83068f09bc1822027b787bf5b32ecf2
Kevin Estrella <kevsestrella@gmail.com>
2020-04-29 07:44:04 +0800

    Implemented sync async inference_requests

--- inference.py
+++ inference.py
@@ -28,15 +28,22 @@ def get_args():
 def async_inference(exec_net, input_blob, image):
     ### TODO: Add code to perform asynchronous inference
     ### Note: Return the exec_net
-
+    exec_net.start_async(request_id=0, inputs={input_blob: image})
+    while True:
+        status = exec_net.requests[0].wait(-1)
+        if status == 0:
+            break
+        else:
+            time.sleep(1)
     return exec_net
 
 
 def sync_inference(exec_net, input_blob, image):
     ### TODO: Add code to perform synchronous inference
+    result = exec_net.infer({input_blob: image})
     ### Note: Return the result of inference
 
-    return None
+    return result
 
 
 def perform_inference(exec_net, request_type, input_image, input_shape):
