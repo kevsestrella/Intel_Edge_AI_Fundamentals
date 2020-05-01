77c28175272aa2cc6e2a0f5cc245ea816947503b
Kevin Estrella <kevsestrella@gmail.com>
2020-05-01 11:36:48 +0800

    Implemented handling_input_streams

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import argparse
 import cv2
 import numpy as np
+from sys import platform
+
+# Get correct video codec
+if platform == "linux" or platform == "linux2":
+    CODEC = 0x00000021
+elif platform == "darwin":
+    CODEC = cv2.VideoWriter_fourcc('M','J','P','G')
+else:
+    print("Unsupported OS.")
+    exit(1)
 
 def get_args():
     '''
@@ -19,18 +29,44 @@ def get_args():
 
 def capture_stream(args):
     ### TODO: Handle image, video or webcam
+    is_image = False
+    if args.i == "CAM":
+        args.i = 0
+    elif args.i.endswith(".jpg") or args.i.endswith(".bmp"):
+        is_image = True
 
     ### TODO: Get and open video capture
+    cap = cv2.VideoCapture(args.i)
+    cap.open(args.i)
 
-    ### TODO: Re-size the frame to 100x100
+    # write a video writer
+    out = None if is_image else cv2.VideoWriter("out.mp4", CODEC, 30, (100,100))
 
-    ### TODO: Add Canny Edge Detection to the frame, 
-    ###       with min & max values of 100 and 200
-    ###       Make sure to use np.dstack after to make a 3-channel image
+    # process video until end or process terminated
+    while cap.isOpened():
+        flag, frame = cap.read()
+        if not flag:
+            break
+        key_pressed = cv2.waitKey(60)
+        ### TODO: Re-size the frame to 100x100
+        frame = cv2.resize(frame, (100, 100))
 
-    ### TODO: Write out the frame, depending on image or video
+        ### TODO: Add Canny Edge Detection to the frame, 
+        ###       with min & max values of 100 and 200
+        ###       Make sure to use np.dstack after to make a 3-channel image
+        frame = cv2.Canny(frame, 100, 200)
+        frame = np.dstack((frame, frame, frame))
+
+        ### TODO: Write out the frame, depending on image or video
+        cv2.imwrite('output_image.jpg', frame) if is_image else out.write(frame)
+        if key_pressed == 27:
+            break
 
     ### TODO: Close the stream and any windows at the end of the application
+    if not is_image:
+        out.release()
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 def main():
